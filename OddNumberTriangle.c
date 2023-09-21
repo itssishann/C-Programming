5136e8af8130f0a405be29c38ac097e1e111ae9f
ISHAN GURJAR <73055623+itssishann@users.noreply.github.com>
2023-09-21 23:28:01 +0530

    Update OddNumberTriangle.c

--- OddNumberTriangle.c
+++ OddNumberTriangle.c
@@ -4,8 +4,8 @@ int main() {
     int n;
     printf("ENTER THE NUMBER\n"); //no. of rows to print
     scanf("%d",&n);
-    for (int i=1;i<=n ;i++ ) { 
-        for(int j=1;j<=i;j++){
+    for (int i=1;i<=n ;i++ ) { // this one is for  row => ----
+        for(int j=1;j<=i;j++){ // this one is for column i.e =>  ||
             printf("%d",(j*2)-1,j); 
             
         }
