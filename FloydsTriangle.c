100f03b89129de76bfb2a4c8d27d27bb086975df
ISHAN GURJAR <73055623+itssishann@users.noreply.github.com>
2023-09-23 23:13:47 +0530

    Add files via upload

---
+++ FloydsTriangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/*OUTUT >>
+ENTER YOUR NUMBER-5
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+int main(){
+    int n;
+    printf("ENTER YOUR NUMBER-");
+    scanf("%d",&n);
+    int a=1;
+    for (int i = 1; i <=n ; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            printf("%d ",a);
+            a++;
+        }
+        printf("\n");
+        
+    }
+    
+}
