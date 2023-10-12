32683e3dcbc27ca564d6f218a656a10cba42a1af
ISHAN GURJAR <73055623+itssishann@users.noreply.github.com>
2023-10-12 07:58:31 +0530

    Create Powerrecursion.c

---
+++ Recursion/Recursion/Powerrecursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int power(int a, int b)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    return a * power(a, b - 1);
+}
+int main()
+{
+    int a;
+    int b;
+    printf("Enter an base value:");
+    scanf("%d", &a);
+
+    printf("Enter an power value:");
+    scanf("%d", &b);
+    printf("%d raised to power %d is ->%d", a, b, power(a, b));
+    return 0;
+}
