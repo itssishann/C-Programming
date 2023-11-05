24ab554f7e228f082750221bb771ef0580232b1e
ISHAN GURJAR <73055623+itssishann@users.noreply.github.com>
2023-11-05 20:38:06 +0530

    Create tranpose_array_rotate_90_deg.c

---
+++ 2d_Arrays/tranpose_array_rotate_90_deg.c
@@ -0,0 +1,56 @@
+// question transpose the array n*n;
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter an Row / Column:");
+    scanf("%d", &n);
+    int arr[n][n];
+    for (int i = 0; i < n; i++)
+    { // input array
+        printf("Enter Values in %d Row", i + 1);
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) // transposing the array
+    {
+        for (int j = i; j < n; j++)
+        {
+          // this are the basics steps to transpose the array..
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
+        }
+    }
+printf("\nArray By User Is ->\n");
+   for (int i = 0; i < n; i++) // printing array by user
+    { 
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",arr[j][i]);
+        }
+        printf("\n");
+    }
+printf("Transpose Array nXn is -> \n"); // printing array transposed
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+       printf("%d ",arr[i][j]);
+    }
+    printf("\n");
+
+}
+  printf("ROTATE 90 DEGREE ARRAY->\n"); 
+  for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+          printf("%d ", arr[n - j - 1][i]);
+      }
+      printf("\n");
+  }
+
+
+    return 0;
+}
